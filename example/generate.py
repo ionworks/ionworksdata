55a5e13399b26adbf60f68f2f03d0c3cfbb75374
rtimms <rob@ionworks.com>
2026-02-09 10:06:21 +0000

    update readme and examples

--- example/generate.py
+++ example/generate.py
@@ -1,187 +1,153 @@
 import pybamm
 import pandas as pd
-from scipy.integrate import cumulative_trapezoid
+import numpy as np
 import pathlib
 import iwutil
-import ionworksdata as iwdata
-
-data_dir = pathlib.Path(__file__).resolve().parent / "data"
 
 
-def save_data_metadata(df, metadata, folder):
-    iwutil.save.csv(df, folder / "data.csv")
-    iwutil.save.json(metadata, folder / "metadata.json")
+def add_constant_current(model, parameter_values, sol=None):
+    V_min = parameter_values["Lower voltage cut-off [V]"]
+    V_max = parameter_values["Upper voltage cut-off [V]"]
 
+    def charge(c_rate):
+        return (
+            pybamm.step.c_rate(-c_rate, termination=f"{V_max} V", period=6),
+            pybamm.step.voltage(V_max, termination="C/50", period=6),
+            pybamm.step.rest(3600),
+        )
 
-def generate_pulse_data(experiment_fn):
-    model = pybamm.lithium_ion.SPM()
-    parameter_values = pybamm.ParameterValues("Chen2020")
-    experiment = experiment_fn(parameter_values)
+    def discharge(c_rate):
+        return (
+            pybamm.step.c_rate(c_rate, termination=f"{V_min} V", period=6),
+            pybamm.step.rest(3600),
+        )
 
+    experiment = pybamm.Experiment(
+        [
+            charge(1 / 5),
+            discharge(1 / 5),
+            charge(1 / 5),
+            discharge(1 / 2),
+            charge(1 / 2),
+            discharge(1),
+            charge(1),
+        ]
+    )
     sim = pybamm.Simulation(
         model, parameter_values=parameter_values, experiment=experiment
     )
-    sol = sim.solve()
-
-    # Save data to csv
-    variables = ["Time [s]", "Current [A]", "Voltage [V]"]
-    df = pd.DataFrame(sol.get_data_dict(variables))
-    df = df.rename(columns={"Cycle": "Cycle number", "Step": "Step number"})
-    df = df.astype({"Cycle number": int, "Step number": int})
-    t = df["Time [s]"].values
-    current = df["Current [A]"].values
-    df["Capacity [A.h]"] = cumulative_trapezoid(current, t / 3600, initial=0)
-
-    try:
-        iwdata.validate.pulse(df)
-        validated = True
-    except AssertionError:
-        validated = False
-
-    return df, validated
+    if sol:
+        sol = sim.solve(starting_solution=sol)
+    else:
+        sol = sim.solve(initial_soc=0.5)
+    return sol
 
 
-def generate_gitt_data():
-    def experiment_fn(parameter_values):
-        C_rate = 0.1
-        pause_s = 10  # 10s
-        step_s = 15 * 60  # 15m
-        rest_s = 15 * 60  # 15m
-        V_min = parameter_values["Lower voltage cut-off [V]"]
+def add_pulse(model, parameter_values, experiment, sol):
+    experiment, max_cycles = experiment
+    sim = pybamm.Simulation(
+        model, parameter_values=parameter_values, experiment=experiment
+    )
+    i = 0
 
-        gitt_experiment = pybamm.Experiment(
-            [
-                (
-                    pybamm.step.rest(pause_s, period=1),
-                    pybamm.step.c_rate(
-                        C_rate,
-                        duration=step_s,
-                        period=step_s / 1000,
-                        termination=f"{V_min} V",
-                    ),
-                    pybamm.step.rest(rest_s, period=rest_s / 100),
-                ),
-            ]
-            * 5
-        )
-        return gitt_experiment
+    # Run until the voltage drops below the lower cut-off
+    # during the second-last step
+    while i == 0 or sol.cycles[-1].steps[-2].termination == "final time":
+        sol = sim.solve(starting_solution=sol)
+        i += 1
+        if i > max_cycles:
+            raise ValueError("Reached maximum number of cycles")
 
-    df, validated = generate_pulse_data(experiment_fn)
-
-    metadata = {
-        "experiment": "gitt",
-        "components": "full",
-        "direction": "discharge",
-        "validated": validated,
-    }
-    save_data_metadata(df, metadata, data_dir / "gitt")
+    return sol
 
 
-def generate_hppt_data():
-    def experiment_fn(parameter_values):
-        # we want the C/3 discharge to increment SOC by 5%
-        # C/3 discharge duration = 3h
-        # 5% SOC increment = 0.05 * 3h = 0.15h
-        discharge_duration = 0.15 * 3600
-        V_min = parameter_values["Lower voltage cut-off [V]"]
-
-        hppt_experiment = pybamm.Experiment(
-            [
-                (
-                    pybamm.step.rest(duration=10, period=1),
-                    pybamm.step.c_rate(1, duration=10, period=0.1),
-                    pybamm.step.rest(duration=300, period=5),
-                    pybamm.step.c_rate(-1, duration=10, period=0.1),
-                    pybamm.step.rest(duration=300, period=5),
-                    pybamm.step.c_rate(2, duration=10, period=0.1),
-                    pybamm.step.rest(duration=300, period=5),
-                    pybamm.step.c_rate(-2, duration=10, period=0.1),
-                    pybamm.step.rest(duration=300, period=5),
-                    pybamm.step.c_rate(
-                        1 / 3,
-                        duration=discharge_duration,
-                        period=discharge_duration / 100,
-                        termination=f"{V_min} V",
-                    ),
-                    pybamm.step.rest(duration=3600, period=10),
-                ),
-            ]
-            * 5
-        )
-        return hppt_experiment
-
-    df, validated = generate_pulse_data(experiment_fn)
-
-    metadata = {
-        "experiment": "hppt",
-        "components": "full",
-        "direction": "discharge",
-        "validated": validated,
-    }
-
-    save_data_metadata(df, metadata, data_dir / "hppt")
-
-
-def generate_ocv_and_rate_capability_data():
-    model = pybamm.lithium_ion.SPM({"thermal": "lumped"})
-    model.variables["Temperature [degC]"] = model.variables[
-        "X-averaged cell temperature [C]"
-    ]
-
-    parameter_values = pybamm.ParameterValues("Chen2020")
+def add_hppt(model, parameter_values, sol):
+    # we want the C/3 discharge to increment SOC by 5%
+    # C/3 discharge duration = 3h
+    # 5% SOC increment = 0.05 * 3h = 0.15h
+    discharge_duration = 0.15 * 3600
+    # V_max = parameter_values["Upper voltage cut-off [V]"]
     V_min = parameter_values["Lower voltage cut-off [V]"]
-    for C_rate in [1 / 50, 1 / 5, 1 / 2, 1, 2]:
-        experiment = pybamm.Experiment(
-            [
+    max_cycles = 25
+
+    hppt_experiment = pybamm.Experiment(
+        [
+            (
                 pybamm.step.c_rate(
-                    C_rate, termination=f"{V_min} V", period=6 * 1 / C_rate
-                )
-            ]
-        )
-        sim = pybamm.Simulation(
-            model, parameter_values=parameter_values, experiment=experiment
-        )
-        sol = sim.solve(initial_soc=1)
-        if C_rate == 1 / 50:
-            df = pd.DataFrame(
-                {
-                    "Capacity [A.h]": sol["Discharge capacity [A.h]"].data,
-                    "Voltage [V]": sol["Voltage [V]"].data,
-                }
-            )
-            metadata = {
-                "experiment": "ocv",
-                "components": "full",
-                "direction": "discharge",
-            }
-            try:
-                iwdata.validate.ocv(df)
-                metadata["validated"] = True
-            except AssertionError:
-                metadata["validated"] = False
-            save_data_metadata(df, metadata, data_dir / "ocv")
-        else:
-            variables = ["Time [s]", "Current [A]", "Voltage [V]", "Temperature [degC]"]
-            df = pd.DataFrame(sol.get_data_dict(variables))
-            df["Capacity [A.h]"] = sol["Discharge capacity [A.h]"].data
-            metadata = {
-                "experiment": "constant current",
-                "components": "full",
-                "direction": "discharge",
-                "C-rate": C_rate,
-            }
-            try:
-                iwdata.validate.constant_current(df)
-                metadata["validated"] = True
-            except AssertionError:
-                metadata["validated"] = False
-            save_data_metadata(df, metadata, data_dir / f"{C_rate}C")
+                    1 / 2, duration=10, period=0.1, termination=f"{V_min} V"
+                ),
+                pybamm.step.rest(duration=300, period=5),
+                pybamm.step.c_rate(
+                    -1 / 2,
+                    duration=10,
+                    period=0.1,  # termination=f"{V_max} V"
+                ),
+                pybamm.step.rest(duration=300, period=5),
+                pybamm.step.c_rate(
+                    1, duration=10, period=0.1, termination=f"{V_min} V"
+                ),
+                pybamm.step.rest(duration=300, period=5),
+                pybamm.step.c_rate(
+                    -1,
+                    duration=10,
+                    period=0.1,  # termination=f"{V_max} V"
+                ),
+                pybamm.step.rest(duration=300, period=5),
+                pybamm.step.c_rate(
+                    1 / 3,
+                    duration=discharge_duration,
+                    period=discharge_duration / 100,
+                    termination=f"{V_min} V",
+                ),
+                pybamm.step.rest(duration=3600, period=10),
+            ),
+        ]
+    )
+    sol = add_pulse(model, parameter_values, (hppt_experiment, max_cycles), sol)
+    return sol
 
 
-print("Generating OCV and rate capability data ...")
-generate_ocv_and_rate_capability_data()
-print("Generating GITT data ...")
-generate_gitt_data()
-print("Generating HPPT data ...")
-generate_hppt_data()
-print("Done.")
+def add_gitt(model, parameter_values, sol):
+    C_rate = -0.1
+    step_s = 30 * 60
+    step_h = step_s / 3600
+    max_cycles = int(1 / abs(C_rate) / step_h * 1.5)
+    V_max = parameter_values["Upper voltage cut-off [V]"]
+
+    gitt_experiment = pybamm.Experiment(
+        [
+            (
+                pybamm.step.c_rate(
+                    C_rate,
+                    duration=step_s,
+                    period=step_s / 100,
+                    termination=f"{V_max} V",
+                ),
+                pybamm.step.rest(3600, period=10),
+            ),
+        ]
+    )
+    sol = add_pulse(model, parameter_values, (gitt_experiment, max_cycles), sol)
+    return sol
+
+
+pybamm.set_logging_level("NOTICE")
+model = pybamm.lithium_ion.SPM({"thermal": "lumped"})
+model.variables["Temperature [degC]"] = model.variables[
+    "X-averaged cell temperature [C]"
+]
+parameter_values = pybamm.ParameterValues("Chen2020")
+sol = add_constant_current(model, parameter_values)
+sol = add_hppt(model, parameter_values, sol)
+sol = add_gitt(model, parameter_values, sol)
+sol = add_constant_current(model, parameter_values, sol)
+sol = add_hppt(model, parameter_values, sol)
+sol = add_gitt(model, parameter_values, sol)
+
+variables = ["Time [s]", "Current [A]", "Voltage [V]", "Temperature [degC]"]
+df = pd.DataFrame(sol.get_data_dict(variables, cycles_and_steps=True))
+# cut-off all max voltages at 4.2 V
+df["Voltage [V]"] = np.minimum(df["Voltage [V]"], 4.2)
+
+data_dir = pathlib.Path(__file__).resolve().parent / "data"
+iwutil.save.csv(df, data_dir / "full_rpt" / "data.csv")
